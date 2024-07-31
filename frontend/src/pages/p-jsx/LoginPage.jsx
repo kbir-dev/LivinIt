47dfc1887a2425e8f5700a13a33692018b59b279
kbir079 <116657420+kbir-dev@users.noreply.github.com>
2024-07-31 21:23:56 +0530

    Update LoginPage.jsx

--- frontend/src/pages/p-jsx/LoginPage.jsx
+++ frontend/src/pages/p-jsx/LoginPage.jsx
@@ -1,16 +1,30 @@
 import React from 'react'
 import "../p-css/LoginPage.css"
 import { Link } from 'react-router-dom'
+import { useState } from 'react'
 
 const LoginPage = () => {
+    const [userInput, setUserInput] = useState({
+        email : '',
+        password : ''
+    })
+
+    function loginUser(){
+        axios.post("/login",{
+            email,
+            password
+        })
+    }
+
+
     return (
         <div className="container-fluid d-flex justify-content-center align-items-center mt-5">
             <div className="col-md-6 col-lg-3">
                 <div className="card border-0 text-center">
                     <div className="card-body d-flex flex-column gap-3">
                         <h3 className="card-title text-center mb-2">Login</h3>
-                        <form className='d-flex flex-column gap-4'>
-                            <input type="email" className="form-control rounded-pill" id="email" placeholder="Enter email" />
+                        <form className='d-flex flex-column gap-4' onSubmit={loginUser}>
+                            <input type="email" className="form-control rounded-pill" id="email" placeholder="Enter email" value={userInput.email}/>
                             <input type="password" className="form-control rounded-pill" id="password" placeholder="Password" />
                             <button type="submit" className="btn root-bg-color btn-block text-white rounded-pill">Login</button>
                         </form>
